c3ef5575b5ff91678f9e3054f8feb8fe5f1803ed
zick <zickminoh@gmail.com>
2014-06-29 12:16:12 +0900

    Add readList and printObj.

--- lisp.scala
+++ lisp.scala
@@ -55,6 +55,8 @@ def makeSym(s: String) = {
   }
 }
 
+val sym_quote = makeSym("quote")
+
 case class Error(obj: Error0) extends LObj
 def makeError(s: String) = Error(new Error0(s))
 
@@ -81,6 +83,20 @@ def makeSubr(f: LObj => LObj) = Subr(new Subr0(f))
 case class Expr(obj: Expr0) extends LObj
 def makeExpr(a: LObj, e: LObj) = Expr(new Expr0(safeCar(a), safeCdr(a), e))
 
+def nreverse(lst: LObj) = {
+  def doit(lst: LObj, ret: LObj): LObj = {
+    lst match {
+      case Cons(c) => {
+        val tmp = c.cdr
+        c.cdr = ret
+        doit(tmp, lst)
+      }
+      case _ => ret
+    }
+  }
+  doit(lst, kNil)
+}
+
 def isDelimiter(c: Char) = {
   c == kLPar || c == kRPar || c == kQuote || c.isWhitespace
 }
@@ -101,32 +117,75 @@ def makeNumOrSym(s: String) = {
   }
 }
 
-def readAtom(s: String) = {
+def readAtom(s: String): (LObj, String) = {
   var i = 0
   while (i < s.length && !isDelimiter(s(i))) {
     i += 1
   }
-  (makeNumOrSym(s), s.substring(i))
+  (makeNumOrSym(s.substring(0, i)), s.substring(i))
 }
 
 def parseError(s: String) = (makeError(s), "")
 
-def read(s: String) = {
+def read(s: String): (LObj, String) = {
   val str = skipSpaces(s)
   if (str.length == 0)
     parseError("empty input")
   else if (str(0) == kRPar)
     parseError("invalid syntax: " + str)
   else if (str(0) == kLPar)
-    parseError("noimpl")
-  else if (str(0) == kQuote)
-    parseError("noimpl")
+    readList(str.substring(1))
+  else if (str(0) == kQuote) {
+    val (elm, next) = read(str.substring(1))
+    (makeCons(sym_quote, makeCons(elm, kNil)), next)
+  }
   else
     readAtom(str)
 }
 
+def readList(s: String) = {
+  def doit(s: String, ret: LObj): (LObj, String) = {
+    if (s.length == 0)
+      parseError("unfinished parenthesis")
+    else if (s(0) == kRPar)
+      (nreverse(ret), s.substring(1))
+    else {
+      val (elm, next) = read(s)
+      elm match {
+        case Error(_) => (elm, next)
+        case _ => doit(skipSpaces(next), makeCons(elm, ret))
+      }
+    }
+  }
+  doit(skipSpaces(s), kNil)
+}
+
+def printObj(obj: LObj): String = {
+  obj match {
+    case Nil(_) => "nil"
+    case Num(n) => n.data.toString
+    case Sym(s) => s.data
+    case Error(e) => "<error: " + e.data + ">"
+    case Cons(_) => printList(obj)
+    case Subr(_) => "<subr>"
+    case Expr(_) => "<expr>"
+  }
+}
+
+def printList(obj: LObj): String = {
+  def doit(obj: LObj, first: Boolean, ret: String): String = {
+    obj match {
+      case Cons(c) =>
+        doit(c.cdr, false, ret + (if (first) "" else " ") + printObj(c.car))
+      case Nil(_) => "(" + ret + ")"
+      case _ => "(" + ret + " . " + printObj(obj) + ")"
+    }
+  }
+  doit(obj, true, "")
+}
+
 print("> ")
 for (line <- Source.stdin.getLines) {
-  println(read(line))
+  println(printObj(read(line)._1))
   print("> ")
 }
